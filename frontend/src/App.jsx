60e3234f781558c91eb2e543268fac3b20036173
Abhishek8915 <abhishekmakwana8915@gmail.com>
2025-03-10 16:44:58 +0530

    first

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='bg-red-100 h-screen flex items-center justify-center'>
+        app      
+    </div>
+  )
+}
+
+export default App
